88bbc3473ae96610bc77baa23273f5bc7ba78082
Eduardo Gutierrez <xgutierrezx7@outlook.com>
2023-12-28 12:15:01 -0300

    trybenchmark character page

--- resources/views/pages/v3/character/about.blade.php
+++ resources/views/pages/v3/character/about.blade.php
@@ -28,14 +28,27 @@
 
         </div>
         <div class="col-md-8">
-
+            @php
+                $time_start = microtime(true);
+            @endphp
             @component('partials.v3.frame', [
                 'title' => 'Sobre ' . $character->name()
             ])
                 @if($character->hasAbout())
                     {!! Markdown::convertToHtml($character->about()) !!}
+                    @php
+                        $time_about = microtime(true);
+                    @endphp
                 @else
                     <em>{{$character->name()}} ainda não possui informações sobre si.</em>
+                    @php
+                        $time_name = microtime(true);
+                        $time_end = microtime(true);
+                        $exec_time_title = ($time_end - $time_start);
+                        $exec_time_about = ($time_end - $time_about);
+                        $exec_time_name = ($time_end - $time_name);
+                    @endphp
+                    <em>Title: {{ $exec_time_title }} <br> about: {{ $exec_time_about }} <br>  name: {{ $exec_time_name }}</em>
                 @endif
 
                 @if(
@@ -51,6 +64,4 @@
 
         </div>
     </div>
-
-
 @stop
